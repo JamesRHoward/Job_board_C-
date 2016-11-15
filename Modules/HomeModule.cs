165366c5686590ec104c749c59ca0de933f4c106
Bryant Wang and James Howard <bryant+james@>
2016-11-15 11:48:20 -0800

    Set up initial files and file structure.

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,13 @@
+using Nancy;
+using System.Collections.Generic;
+
+namespace JobBoard
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => View["index.cshtml"];
+    }
+  }
+}
